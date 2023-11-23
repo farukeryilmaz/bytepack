3ded19abc840947831372bdcc26b6583a475b004
Faruk Eryilmaz <frkerylmz@gmail.com>
2023-11-23 23:40:04 +0300

    remove immutable buffer and rename buffer_view to buffer. remove compile-time buffer size check.

--- include/bytepack/bytepack.hpp
+++ include/bytepack/bytepack.hpp
@@ -5,100 +5,23 @@
 #include <string>
 #include <cstring> 
 #include <cstddef>
+#include <cstdint>
 
 namespace bytepack {
 
-	class buffer final
-	{
+	class buffer {
 	public:
-		explicit buffer() noexcept
-			: data_{ nullptr }, size_{ 0 }, ssize_{ 0 }
-		{};
-
-		explicit buffer(const std::size_t size) noexcept
-			: data_{ new char[size] {} }, size_{ size }
-		{
-			if (size <= static_cast<std::size_t>(std::numeric_limits<std::ptrdiff_t>::max())) {
-				ssize_ = static_cast<ptrdiff_t>(size);
-			}
-			else {
-				ssize_ = std::numeric_limits<std::ptrdiff_t>::max();
-			}
-		}
-
-		~buffer() noexcept {
-			delete[] static_cast<char*>(data_);
-		}
-
-		buffer(const buffer& other)
-			: data_{ new char[other.size_] }, size_{ other.size_ }, ssize_{ other.ssize_ } {
-			std::memcpy(data_, other.data_, size_);
-		}
-
-		buffer(buffer&& other) noexcept
-			: data_{ other.data_ }, size_{ other.size_ }, ssize_{ other.ssize_ } {
-			other.data_ = nullptr;
-			other.size_ = 0;
-			other.ssize_ = 0;
-		}
-
-		buffer& operator=(const buffer& other) {
-			if (this != &other) {
-				delete[] static_cast<char*>(data_);
-				size_ = other.size_;
-				ssize_ = other.ssize_;
-				data_ = new char[size_];
-				std::memcpy(data_, other.data_, size_);
-			}
-			return *this;
-		}
-
-		buffer& operator=(buffer&& other) noexcept {
-			if (this != &other) {
-				delete[] static_cast<char*>(data_);
-				data_ = other.data_;
-				size_ = other.size_;
-				ssize_ = other.ssize_;
-				other.data_ = nullptr;
-				other.size_ = 0;
-				other.ssize_ = 0;
-			}
-			return *this;
-		}
-
-		void* data() const { return data_; }
-
-		std::size_t size() const { return size_; }
-
-		std::ptrdiff_t ssize() const { return ssize_; }
-
-		operator bool() const {
-			return data_ && size_ > 0;
-		}
-
-	private:
-		void* data_;
-		std::size_t size_;		// unsigned
-		std::ptrdiff_t ssize_;	// signed
-	};
-
-	class buffer_view final {
-	public:
-		buffer_view(const buffer& buf)
-			: data_{ buf.data() }, size_{ buf.size() }, ssize_{ buf.ssize() }
-		{}
-
-		template<typename T, size_t N>
-		buffer_view(T(&array)[N])
+		template<typename T, std::size_t N>
+		buffer(T(&array)[N])
 			: data_{ array }, size_{ N * sizeof(T) },
 			ssize_{ get_ssize(N * sizeof(T)) } {}
 
 		template<typename T>
-		buffer_view(T* ptr, size_t size)
+		buffer(T* ptr, std::size_t size)
 			: data_{ static_cast<void*>(ptr) }, size_{ size * sizeof(T) },
 			ssize_{ get_ssize(size * sizeof(T)) } {}
 
-		buffer_view(std::string& str)
+		buffer(std::string& str)
 			: data_{ str.data() }, size_{ str.size() }, ssize_{ get_ssize(str.size()) } {}
 
 		void* data() const { return data_; }
@@ -151,27 +74,46 @@ namespace bytepack {
 		std::ptrdiff_t ssize_;	// signed
 	};
 
-	template<int BufferSize, std::endian OutputEndian = std::endian::native>
-	class binary_stream final {
-		static_assert(("Buffer size must be greater than 0 (zero)", BufferSize > 0));
+	template<std::endian TargetEndian = std::endian::big>
+	class binary_stream {
 
 	public:
-		constexpr explicit binary_stream() noexcept
-			: system_endianness_{ std::endian::native }, target_endianness_{ OutputEndian }
+		explicit binary_stream(const std::size_t buffer_size) noexcept
+			: system_endianness_{ std::endian::native }, target_endianness_{ TargetEndian },
+			buffer_{ new std::uint8_t[buffer_size]{}, buffer_size }, owns_buffer_{ true }
 		{
-			buffer_ = bytepack::buffer(static_cast<std::size_t>(BufferSize));
+			// TODO: consider lazy initialization (lazy-load) for `buffer_`. It might come in useful in some cases.
 		}
 
-		bytepack::buffer_view data() const {
+		explicit binary_stream(const bytepack::buffer& buffer) noexcept
+			: system_endianness_{ std::endian::native }, target_endianness_{ TargetEndian },
+			buffer_{ buffer }, owns_buffer_{ false }
+		{}
+
+		~binary_stream() noexcept {
+			if (owns_buffer_) {
+				delete[] buffer_.as<std::uint8_t>();
+			}
+		}
+
+		bytepack::buffer data() const {
 			return buffer_;
 		}
+
+		binary_stream(const binary_stream&) = delete;
+		binary_stream& operator=(const binary_stream&) = delete;
+		binary_stream(binary_stream&&) = delete;
+		binary_stream& operator=(binary_stream&&) = delete;
 
 	private:
 		std::endian system_endianness_;
 		std::endian target_endianness_;
 
-		// TODO: consider lazy initialization (lazy-load). It might come in useful in some cases.
 		bytepack::buffer buffer_;
+
+		// Flag to indicate buffer ownership
+		// TODO: Consider alternative ownership models and design to handle external buffers
+		bool owns_buffer_;
 	};
 
 }
