128fc6a0e91bdb73b551bd6d1bf9c380cd69d574
Faruk Eryilmaz <frkerylmz@gmail.com>
2023-11-26 17:40:56 +0300

    add serialize/deserialize test for std::string and std::string_view

--- test/test_bytepack.cpp
+++ test/test_bytepack.cpp
@@ -241,3 +241,105 @@ TEST_CASE("Fundamental types (little-endian)")
 	REQUIRE(num17 == num17_);
 	REQUIRE(num18 == num18_);
 }
+
+TEST_CASE("String and sting view (big-endian)")
+{
+	std::string str = "Hello World!";
+	std::string_view str2 = "bytepack library";
+
+	bytepack::binary_stream bstream(1024);
+
+	bstream.write(str);
+	bstream.write<short int>(str2);
+
+	std::string str_{};
+	std::string str2_{}; // string_view cannot be read into directly (it's a reference)
+
+	auto buffer = bstream.data();
+	bytepack::binary_stream bstream_(buffer);
+
+	bstream_.read(str_);
+	bstream_.read<short int>(str2_);
+
+	// 8 bytes for the size of the string, 12 bytes for the string itself
+	// 2 bytes for the custom size of the string_view, 16 bytes for the string_view itself
+	// 38 bytes in total
+	REQUIRE(38 == buffer.size());
+
+	REQUIRE(str == str_);
+	REQUIRE(str2 == str2_);
+}
+
+TEST_CASE("String and sting view (little-endian)")
+{
+	std::string str = "Hello Bytepack!";
+	std::string_view str2 = "bytepack library C++20";
+
+	bytepack::binary_stream<std::endian::little> bstream(64);
+
+	bstream.write(str);
+	bstream.write<unsigned long int>(str2);
+
+	std::string str_{};
+	std::string str2_{}; // string_view cannot be read into directly (it's a reference)
+
+	auto buffer = bstream.data();
+	bytepack::binary_stream<std::endian::little> bstream_(buffer);
+
+	bstream_.read(str_);
+	bstream_.read<unsigned long int>(str2_);
+
+	// 8 bytes for the size of the string, 15 bytes for the string itself
+	// 4 bytes for the custom size of the string_view, 22 bytes for the string_view itself
+	// 49 bytes in total
+	REQUIRE(49 == buffer.size());
+
+	REQUIRE(str == str_);
+	REQUIRE(str2 == str2_);
+}
+
+TEST_CASE("String, sting view and fundamental types mixed (big-endian)")
+{
+	double num1 = 3754.34526243;
+	std::string str = "Hello World!";
+	std::uint32_t num2 = 9034;
+	std::string_view str2 = "bytepack library";
+	float num3 = 193.92834f;
+
+	bytepack::binary_stream bstream(1024);
+
+	bstream.write(num1);
+	bstream.write(str);
+	bstream.write(num2);
+	bstream.write<short int>(str2);
+	bstream.write(num3);
+
+	double num1_{};
+	std::string str_{};
+	std::uint32_t num2_{};
+	std::string str2_{}; // string_view cannot be read into directly (it's a reference)
+	float num3_{};
+
+	auto buffer = bstream.data();
+	bytepack::binary_stream bstream_(buffer);
+
+	bstream_.read(num1_);
+	bstream_.read(str_);
+	bstream_.read(num2_);
+	bstream_.read<short int>(str2_);
+	bstream_.read(num3_);
+
+	// 8 bytes for double,
+	// 8 bytes for the size of the string, 12 bytes for the string itself,
+	// 4 bytes for the uint32_t,
+	// 2 bytes for the custom size of the string_view, 16 bytes for the string_view itself,
+	// 4 bytes for the float
+	// 54 bytes in total
+	REQUIRE(54 == buffer.size());
+
+	REQUIRE(num1 == Approx(num1_).epsilon(1e-7));
+	REQUIRE(str == str_);
+	REQUIRE(num2 == num2_);
+	REQUIRE(str2 == str2_);
+	REQUIRE(num3 == Approx(num3_).epsilon(1e-4));
+}
